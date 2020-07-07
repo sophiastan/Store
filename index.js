46554ae8a4e575f8861d3a69c568306746af75ae
Sophia Tan <sophia.tan.ad@gmail.com>
2020-07-07 16:41:14 -0700

    fixed port

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.get('/', (req, res) => {
     res.send({ bye: 'buddy' });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(port);
+const PORT = process.env.PORT || 6000;
+app.listen(PORT);
